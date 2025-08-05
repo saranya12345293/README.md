9127b24c22591efa5b5a49249b44853ad5a8fbc1
saranya12345293 <saranyavsaranyav1@gmail.com>
2025-08-05 06:15:02 -0700

    Add files via upload

---
+++ ticketing_project.sql
@@ -0,0 +1,17 @@
+CREATE TABLE tickets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100),
+    issue_description TEXT,
+    status VARCHAR(50)
+);
+
+
+CREATE TABLE tickets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100),
+    issue_description TEXT,
+    status VARCHAR(50)
+);
+
+SELECT * FROM tickets;
+DESCRIBE tickets;
